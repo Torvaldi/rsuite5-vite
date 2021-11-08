976ad05732806b42154dc9b479996765e486cf44
Quentin 'Torvald' LAURENT <ql2697@gmail.com>
2021-11-08 18:41:26 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button, ButtonToolbar, Modal, Placeholder } from "rsuite";
+
+export const App = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <div className="modal-container">
+      <ButtonToolbar>
+        <Button size="lg" onClick={handleOpen}>
+          Open
+        </Button>
+      </ButtonToolbar>
+      <Modal full open={open} onClose={handleClose}>
+        <Modal.Header>
+          <Modal.Title>Modal Title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Placeholder.Paragraph rows={8} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleClose} appearance="primary">
+            Ok
+          </Button>
+          <Button onClick={handleClose} appearance="subtle">
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+export default App;
